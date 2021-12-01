2f631ff190510587e8adcc6d8872b490c2a723c7
GeneralGresi <general@gresihome.at>
2021-12-01 11:59:48 +0100

    Change to RotatingFileHandler, create custom loglevel "Verbose"

--- pzm_common.py
+++ pzm_common.py
@@ -5,6 +5,8 @@ import datetime
 import sys
 import logging
 
+from logging.handlers import RotatingFileHandler
+
 def initialize(name, enable_logging=True):
     global debug
     global verbose
@@ -12,33 +14,45 @@ def initialize(name, enable_logging=True):
     global statusJsonFile
     global considered_empty
     global logger
+    global log_to_file
     debug = False
     verbose = False
     test = False
     statusJsonFile = "/var/lib/pve-zsync/manager_sync_state"
     considered_empty = ['\n', '', " "]
-    if enable_logging:
-        logging.basicConfig(filename=f"/var/log/pzm_{name}.log", format="%(asctime)s - %(levelname)s - %(message)s", encoding="utf-8", level=logging.DEBUG)
+
+    #Logging
+    logging.addLevelName(logging.DEBUG + 5, "VERBOSE")
+    logging.VERBOSE = logging.DEBUG + 5
+    log_file_handler = RotatingFileHandler(filename=f"/var/log/pzm_{name}.log", encoding="utf-8", maxBytes=10485760, backupCount=10)
+    log_file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    log_file_handler.setFormatter(log_file_formatter)
+
     logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    if enable_logging:
+        logger.addHandler(log_file_handler)
 
 #Log to stdout
-def log(data, severity = logging.INFO):        
+def log(data, severity = logging.INFO):
     global logger
+    global log_to_file
     if severity == logging.DEBUG:
         print ("DEBUG - " + str(data))
     else:
         print(str(data))
     logger.log(severity, data)
 
-#Log to stdout if global debug variable is set
+#Log to stdout if global debug or verbose variable is set
 def log_verbose(data):
     global verbose
     global debug
     global logger
     if verbose or debug:
-        log(str(data))
+        log(str(data), logging.VERBOSE)
     else:
-        logger.log(logging.INFO, data)
+        logger.log(logging.VERBOSE, data)
+
 #Log to stdout if global debug variable is set
 def log_debug(data):
     global debug
@@ -47,7 +61,7 @@ def log_debug(data):
         log(str(data), logging.DEBUG)
     else:
         logger.log(logging.DEBUG, data)
-    
+
 #Execute command will not alter anything. These commands can be executed as normal in "TEST" mode
 def execute_readonly_command(command):
     log_debug ("Executing command: " + " ".join(command))
